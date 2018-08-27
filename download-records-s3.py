2654053bac6f47396f5a6828c9b24b45fb942e52
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2018-08-27 19:56:25 +0200

    Ensure errors are logged

--- download-records-s3.py
+++ download-records-s3.py
@@ -18,17 +18,20 @@ import listRecords
 import download
 
 def main():
-    dataset = 'ictrp-raw-{}-w{:02d}.xml.gz'.format(*datetime.datetime.today().isocalendar())
-    idlist = listRecords.allList()
-    with tempfile.TemporaryFile() as tmpfile:
-        with gzip.GzipFile('raw.xml.gz', 'w', 9, tmpfile) as outfile:
-            failed = download.downloadRecords(idlist, outfile, True)
-            logger.info("Failed (all attempts): {}".format(str(failed)))
-        tmpfile.seek(0)
-        # write to s3
-        s3 = boto3.resource('s3')
-        object = s3.Bucket('ictrp-data').put_object(Key=dataset, Body=tmpfile)
-        object.Acl().put(ACL='public-read')
+    try:
+        dataset = 'ictrp-raw-{}-w{:02d}.xml.gz'.format(*datetime.datetime.today().isocalendar())
+        idlist = listRecords.allList()
+        with tempfile.TemporaryFile() as tmpfile:
+            with gzip.GzipFile('raw.xml.gz', 'w', 9, tmpfile) as outfile:
+                failed = download.downloadRecords(idlist, outfile, True)
+                logger.info("Failed (all attempts): {}".format(str(failed)))
+            tmpfile.seek(0)
+            # write to s3
+            s3 = boto3.resource('s3')
+            object = s3.Bucket('ictrp-data').put_object(Key=dataset, Body=tmpfile)
+            object.Acl().put(ACL='public-read')
+    except:
+        logging.error("ICTRP download failed", exc_info=True)
 
 if __name__ == "__main__":
     main()
