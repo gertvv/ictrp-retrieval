d5a744fe5517ab3a0ad89a4348a594baab4a081f
Gert van Valkenhoef <gert@gertvv.nl>
2020-04-17 20:50:33 +0200

    Fixes

--- awsSecrets.py
+++ awsSecrets.py
@@ -8,14 +8,10 @@ def getSecrets():
     if __SECRETS__ is not None:
         return __SECRETS
 
-    if os.environ['AWS_SECRET_NAME']:
+    if 'AWS_SECRET_NAME' in os.environ:
         # Create a Secrets Manager client
         session = boto3.session.Session()
         client = session.client(service_name='secretsmanager')
-
-        # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-        # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        # We rethrow the exception by default.
 
         get_secret_value_response = client.get_secret_value(SecretId=os.environ['AWS_SECRET_NAME'])
         secret = get_secret_value_response['SecretString']
